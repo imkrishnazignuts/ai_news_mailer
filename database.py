7bd127882c39bb23b7d387785d9a4e5377c6c997
imkrishnazignuts <krishnaz@zignuts.com>
2026-05-22 15:02:09 +0530

    database config changed

--- database.py
+++ database.py
@@ -3,12 +3,28 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
 load_dotenv()
 
 URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(url=URL)
+if not URL:
+    raise RuntimeError("DATABASE_URL environment variable is not set")
+
+if URL.startswith("postgres://"):
+    URL = URL.replace("postgres://", "postgresql://", 1)
+
+connect_args = {}
+if "sslmode=" not in URL:
+    connect_args["sslmode"] = "require"
+
+engine = create_engine(
+    url=URL,
+    connect_args=connect_args,
+    poolclass=NullPool,
+    pool_pre_ping=True,
+)
 
 Session_local = sessionmaker(
     bind=engine,
